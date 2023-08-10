f3534a89242b352c51010ede2d03da1c8a97f3f8
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-10 15:39:35 +0330

    Add files via upload

---
+++ ConsoleApp87/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp87
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            int z;
+            int result;
+
+            Console.Write("Enter first integer:");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Enter second integer:");
+            y = int.Parse(Console.ReadLine());
+            Console.Write("Enter third integer:");
+            z = int.Parse(Console.ReadLine());
+
+            result = x * y * z;
+
+            Console.WriteLine("product is{0}",result);
+            Console.ReadLine();
+        }
+    }
+}
